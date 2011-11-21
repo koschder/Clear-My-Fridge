56ca587640da463f4a7899f500c262f4e58f8d8e
Lukas Kuster <koschder@gmail.com>
2011-11-21 19:27:10 +0100

    Test for RecipeRepository

---
+++ ClearMyFridge/src/test/java/ch/bfh/cmf/repositories/RecipeRepositoryTest.java
@@ -0,0 +1,51 @@
+package ch.bfh.cmf.repositories;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import ch.bfh.cmf.domain.Recipe;
+import ch.bfh.cmf.domain.User;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:repository-context.xml")
+public class RecipeRepositoryTest {
+
+	@Autowired
+	private RecipeRepository recipeRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Test
+	public void testSaveNewRecipe() {
+		User author = userRepository.save(new User("author"));
+
+		Recipe recipe = new Recipe();
+		recipe.setAuthor(author);
+		recipe.setDescription("description");
+		recipe.setName("delicious cupcakes");
+		recipe.setPicture("/path/to/img");
+		recipeRepository.save(recipe);
+
+		assertEquals(recipe, recipeRepository.findAll().iterator().next());
+	}
+
+	@Test
+	public void testSaveRecipeWithoutAuthor() {
+		recipeRepository.save(new Recipe());
+	}
+
+	@Test(expected = InvalidDataAccessApiUsageException.class)
+	public void testSaveRecipeWithTransientAuthor() {
+		Recipe recipe = new Recipe();
+		recipe.setAuthor(new User("transient"));
+		recipeRepository.save(recipe);
+	}
+
+}
