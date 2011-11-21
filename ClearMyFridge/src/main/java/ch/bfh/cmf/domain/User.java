b878dfee915d69f82ad0ccf50d16bb318418207e
Lukas Kuster <koschder@gmail.com>
2011-11-21 07:55:53 +0100

    enable jpa nature

--- ClearMyFridge/src/main/java/ch/bfh/cmf/domain/User.java
+++ ClearMyFridge/src/main/java/ch/bfh/cmf/domain/User.java
@@ -1,6 +1,37 @@
 package ch.bfh.cmf.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	@Id
+	private long id;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	private String name;
 	private String email;
 }
