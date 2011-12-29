2e95776d825dbff897f6ae47f7f590f09978c9ce
Lukas Kuster <koschder@gmail.com>
2011-12-29 22:41:40 +0100

    test saving recipe with ingredients

--- ch.clearmyfridge.service/src/test/java/ch/bfh/cmf/repositories/RecipeRepositoryTest.java
+++ ch.clearmyfridge.service/src/test/java/ch/bfh/cmf/repositories/RecipeRepositoryTest.java
@@ -1,9 +1,15 @@
 package ch.bfh.cmf.repositories;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,17 +18,15 @@ import ch.bfh.cmf.domain.Ingredient;
 import ch.bfh.cmf.domain.Recipe;
 import ch.bfh.cmf.domain.User;
 
-import static org.junit.Assert.*;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:test-application-context.xml")
 @Transactional
 public class RecipeRepositoryTest {
 
-	@Autowired
+	@Inject
 	private RecipeRepository recipeRepository;
 
-	@Autowired
+	@Inject
 	private UserRepository userRepository;
 
 	@Test
@@ -37,6 +41,7 @@ public class RecipeRepositoryTest {
 		recipeRepository.save(recipe);
 
 		assertEquals(recipe, recipeRepository.findAll().iterator().next());
+		assertEquals(1, recipeRepository.findAll().size());
 	}
 
 	@Test
@@ -44,21 +49,37 @@ public class RecipeRepositoryTest {
 		recipeRepository.save(new Recipe());
 	}
 
-	@Test(expected = InvalidDataAccessApiUsageException.class)
+	@Test
 	public void testSaveRecipeWithTransientAuthor() {
 		Recipe recipe = new Recipe();
-		recipe.setAuthor(new User("transient"));
-		recipeRepository.save(recipe);
+		final User transientAuthor = new User("transient");
+		recipe.setAuthor(transientAuthor);
+		recipe = recipeRepository.save(recipe);
+		assertEquals(transientAuthor, recipe.getAuthor());
+		assertNull("The user must not have been persisted", recipe.getAuthor().getId());
 	}
-	
+
 	@Test
-	public void testLoadRecipeWithIngredients()
-	{
+	public void testSaveRecipeWithIngredients() {
+		Recipe recipe = new Recipe();
+		recipe.setName("PfefferRezept");
+		final Ingredient pepper = new Ingredient("Pfeffer");
+		recipe.addIngredient(pepper, 2, "tsp");
+		recipeRepository.save(recipe);
+
+		List<Recipe> recipes = recipeRepository.findAll();
+		assertEquals(1, recipes.size());
+		assertTrue(recipes.contains(recipe));
+		assertEquals("Pfeffer", recipes.get(0).getIngredients().iterator().next().getIngredient().getName());
+	}
+
+	@Test
+	public void testLoadRecipeWithIngredients() {
 		Recipe recipe = new Recipe();
 		recipe.setName("PfefferRezept");
 		recipe.addIngredient(new Ingredient("Pfeffer"), 2, "tsp");
 		recipeRepository.save(recipe);
-		
+
 		recipe = recipeRepository.findByName("PfefferRezept");
 		assertEquals("Pfeffer", recipe.getIngredients().iterator().next().getIngredient().getName());
 	}
