563231284313c2e958aa1ca3bef6413d8dbe9e4d
Lukas Kuster <koschder@gmail.com>
2011-12-29 17:16:57 +0100

    create findByRecipe method on RatingRepository

--- ch.clearmyfridge.service/src/main/java/ch/bfh/cmf/repositories/RatingRepository.java
+++ ch.clearmyfridge.service/src/main/java/ch/bfh/cmf/repositories/RatingRepository.java
@@ -1,11 +1,14 @@
 package ch.bfh.cmf.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import ch.bfh.cmf.domain.Rating;
+import ch.bfh.cmf.domain.Recipe;
 
 @Repository
 public interface RatingRepository extends JpaRepository<Rating, Long> {
-
+	public List<Rating> findByRecipe(Recipe recipe);
 }
