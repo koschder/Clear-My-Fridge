0d11c2555b4db1cbb335773c92159ad208a26161
Stefan Heinemann <sh@adfinis.com>
2012-01-10 19:23:06 +0100

    Implemented the adding and removing of ingredients to recipes
    Not quite working since the IngredientConverter is not fully implemented

---
+++ ch.clearmyfridge.web/src/main/java/ch/bfh/cmf/converter/IngredientConverter.java
@@ -0,0 +1,26 @@
+package ch.bfh.cmf.converter;
+
+import java.util.ArrayList;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+import ch.bfh.cmf.domain.Ingredient;
+
+@FacesConverter("ingredientConverter")
+public class IngredientConverter implements Converter {
+
+	@Override
+	public Object getAsObject(FacesContext arg0, UIComponent arg1, String arg2) {
+		// 
+		return 2;
+	}
+
+	@Override
+	public String getAsString(FacesContext arg0, UIComponent arg1, Object arg2) {
+		return "" + ((Ingredient) arg2).getId();
+	}
+
+}
