23bda5a56bf7d69ca8cf25f3f3388f93d90931bc
Lukas Kuster <koschder@gmail.com>
2012-01-10 17:55:41 +0100

    Started work on search result list

--- ch.clearmyfridge.web/src/main/java/ch/bfh/cmf/web/SearchResultsPageBean.java
+++ ch.clearmyfridge.web/src/main/java/ch/bfh/cmf/web/SearchResultsPageBean.java
@@ -1,9 +1,64 @@
 package ch.bfh.cmf.web;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.springframework.context.annotation.Scope;
+
+import ch.bfh.cmf.domain.Ingredient;
+import ch.bfh.cmf.domain.Recipe;
+import ch.bfh.cmf.services.RecipeService;
+
+@Named("searchResultPageBean")
+@Scope("session")
 public class SearchResultsPageBean {
 
-	public SearchResultsPageBean() {
-		
+	@Inject
+	private RecipeService recipeService;
+	private Collection<Ingredient> ingredients = new ArrayList<Ingredient>();
+
+	private Ingredient ingredient;
+	private String ingredientName;
+
+	public List<Recipe> getRecipes() {
+		if (ingredients.isEmpty())
+			return new ArrayList<Recipe>();
+		return recipeService.findRecipesUsing(ingredients);
 	}
 
+	public Collection<Ingredient> getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredient(Ingredient ingredient) {
+		this.ingredient = ingredient;
+	}
+
+	public Ingredient getIngredient() {
+		return ingredient;
+	}
+
+	public void addIngredient() {
+		ingredients.add(new Ingredient(ingredientName));
+	}
+
+	public void removeIngredient() {
+		ingredients.remove(ingredient);
+	}
+
+	public void setIngredients(Collection<Ingredient> ingredients) {
+		this.ingredients = ingredients;
+	}
+
+	public String getIngredientName() {
+		return ingredientName;
+	}
+
+	public void setIngredientName(String ingredientName) {
+		this.ingredientName = ingredientName;
+	}
 }
